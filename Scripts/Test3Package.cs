3181fdfb80dfa76988d1d3c485166a1f07b37ea2
Nisha DEVI <nisha.devi@robertbird.com.au>
2025-04-07 09:20:25 +1000

    First commit

---
+++ Scripts/Test3Package.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RBG.Test3
+{
+    public class Test3Package : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("Test 3 package.");
+        }
+    }
+}
